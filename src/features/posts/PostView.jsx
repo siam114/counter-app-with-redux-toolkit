d508db291a5adbbcd44d58dde30812ac4ee27d44
siam114 <smsiamislam598@gmail.com>
2025-05-12 16:45:09 +0600

    dispatch

--- src/features/posts/PostView.jsx
+++ src/features/posts/PostView.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from './postSlice';
 
 const PostView = () => {
     const {isLoading, posts, error} = useSelector((state) => state.posts);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+       dispatch(fetchPosts())
+    }, [dispatch])
+
   return (
     <div>
         {isLoading && <h2>Loading...</h2>}
